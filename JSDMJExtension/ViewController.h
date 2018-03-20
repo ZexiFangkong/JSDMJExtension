0d119622f581b07316e1c8312618d26e9dd73ab4
Jersey <jersey.li@yeamoney.cn>
2018-03-20 14:08:51 +0800

    fix #1，初始化工程
    1. 初始化工程。
    2. Pod引入MJ、AF三方库、并将其设为忽略文件。

---
+++ JSDMJExtension/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSDMJExtension
+//
+//  Created by jersey on 20/3/18.
+//  Copyright © 2018年 YeamoneyCafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSDMJExtension/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JSDMJExtension
+//
+//  Created by jersey on 20/3/18.
+//  Copyright © 2018年 YeamoneyCafe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
